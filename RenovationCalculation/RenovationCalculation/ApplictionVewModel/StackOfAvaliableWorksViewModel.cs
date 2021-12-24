19c6988f185e6d3da122b211ed003130aa748fb4
viavital <viavital@ukr.net>
2021-12-24 23:26:09 +0200

    binding

--- RenovationCalculation/RenovationCalculation/ApplictionVewModel/StackOfAvaliableWorksViewModel.cs
+++ RenovationCalculation/RenovationCalculation/ApplictionVewModel/StackOfAvaliableWorksViewModel.cs
@@ -11,31 +11,41 @@ using System.Data.Entity;
 
 namespace RenovationCalculation.ApplictionVewModel
 {
-    class StackOfAvaliableWorksViewModel
+    class StackOfAvaliableWorksViewModel : INotifyPropertyChanged
     {
         private TypeOfWorkModel SelectedTypeOfWork;
-        public List<TypeOfWorkModel> TypeOfWorks { get; set; }
-
-        public StackOfAvaliableWorksViewModel()
-        {
-            using (WorksDBContext db = new WorksDBContext())
-            {
-                
-                TypeOfWorks = db.Works.ToList();
-            }
-        }
-         TypeOfWorkModel SelectedWork
+        public TypeOfWorkModel SelectedWork
         {
             get { return SelectedTypeOfWork; }
             set
             {
                 SelectedTypeOfWork = value;
-                OnPropertyChanged("SelectedPhone");
+                OnPropertyChanged("SelectedTypeOfWork");
             }
         }
 
-       
+        private WorkerModel SelectedWorker;
+        public WorkerModel selectedWorker
+        {
+            get { return SelectedWorker; }
+            set
+            {
+                SelectedWorker = value;
+                OnPropertyChanged("SelectedWorker");
+            }
+        }
 
+        public List<TypeOfWorkModel> TypeOfWorks { get; set; }
+        public List<WorkerModel> Workers { get; set; }
+
+        public StackOfAvaliableWorksViewModel()
+        {
+            using (WorksDBContext db = new WorksDBContext())
+            {
+                Workers = db.Workers.ToList();
+                TypeOfWorks = db.Works.ToList();
+            }
+        }
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string prop = "")
         {
