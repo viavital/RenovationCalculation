43984dbcbf8f9e120a76772b0dda097d0631063c
viavital <viavital@ukr.net>
2021-12-24 06:20:00 +0200

    24.12 thinking about view

--- RenovationCalculation/RenovationCalculation/Model/TypeOfWorkModel.cs
+++ RenovationCalculation/RenovationCalculation/Model/TypeOfWorkModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,8 +11,9 @@ namespace RenovationCalculation.Model
     class TypeOfWorkModel
     {
         public int ID { get; set; }
-        public string TypeOfWorkName { get; set; }
-        public int QuantityHoursOfWork { get; set; }     
+        public string typeOfWorkName { get; set; }
+        public int QuantityHoursOfWork { get; set; }
         public int TotalPriceOfWork { get; set; }
+
     }
 }
