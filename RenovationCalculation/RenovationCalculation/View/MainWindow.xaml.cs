8c9fe7c6b49d2f74be5f60f4fe00744b6d262ff4
viavital <viavital@ukr.net>
2022-01-09 18:45:53 +0200

    Refreshing DataBase remade

--- RenovationCalculation/RenovationCalculation/View/MainWindow.xaml.cs
+++ RenovationCalculation/RenovationCalculation/View/MainWindow.xaml.cs
@@ -1,5 +1,22 @@
-﻿using RenovationCalculation.ApplictionViewModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RenovationCalculation.Model;
+using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using RenovationCalculation.ApplictionViewModel;
+using RenovationCalculation.View;
 
 namespace RenovationCalculation
 {
@@ -8,11 +25,30 @@ namespace RenovationCalculation
     /// </summary>
     public partial class MainWindow : Window
     {
-        //v: тут код не пишемо, якщо виникає потреба щось тут писати - треба шукати проблему.
+        StackOfAddingWorksViewModel stackOfAddingWorksViewModel = new();
+        Adding_a_new_worker adding_A_New_Worker;
         public MainWindow()
         {
-            DataContext = new StackOfAddingWorksViewModel();
+            DataContext = stackOfAddingWorksViewModel;
             InitializeComponent();
+            stackOfAddingWorksViewModel.AddNewWorkerEvent += AddNewWorkerEventHandler;
         }
+
+        private void Adding_A_New_Worker_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            adding_A_New_Worker.Closing -= Adding_A_New_Worker_Closing;
+            stackOfAddingWorksViewModel.isEnabledMainWindow = true;            
+        }
+
+        private void AddNewWorkerEventHandler()
+        {
+            {
+                stackOfAddingWorksViewModel.isEnabledMainWindow = false;
+                adding_A_New_Worker = new();
+                adding_A_New_Worker.Show();
+                adding_A_New_Worker.Closing += Adding_A_New_Worker_Closing;
+            }
+        }
+
     }
 }
